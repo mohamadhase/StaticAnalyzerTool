d2cc7179c8a389c401dbc71e15ed5e69b2ec84fc
mohamad nasser <mohamadnasser198@outlook.com>
2022-03-04 19:49:35 +0200

    requested changes applyed

---
+++ Data.py
@@ -0,0 +1,67 @@
+from Const import Const
+
+
+class Data:
+    """
+    class for read the data from the txt file and get it ready to be used in other classes
+    """
+
+    def __init__(self, file_name):
+        """
+        1) read the data from text file 
+        2) split the data into lines and store it in lines variable
+        3) initialize tokens dictinary to store tha type of each line in it
+        Args:
+            file_name (String): the name of text file contains the c++ source code to be checked
+        """
+        self.data = open(file_name).read()
+        self.lines = self.data.split("\n")
+        self.tokens = {}
+
+    def tokinizer(self) ->None:
+        """
+        this function give each line in the source code token to recognize it 
+        """
+        for line in self.lines:
+            if line.startswith("//"):
+                self.tokens[line] = {"Type": "Comment"}
+
+            elif line.startswith("class"):
+                self.tokens[line] = {"Type": "Class"}
+
+            elif any(map(line.startswith,
+                         Const._data_type)) and line.endswith(")"):
+                self.tokens[line] = {"Type": "Function"}
+
+            elif line.startswith("cout <<") or line.startswith("cout<<"):
+                self.tokens[line] = {"Type": "Print"}
+
+            elif any(map(line.startswith,
+                         Const._data_type)) and (line.__contains__("=")
+                                                 or line.__contains__(";")):
+                self.tokens[line] = {"Type": "Declaration"}
+                self.declarationToken(line)
+
+            elif any(map(line.__contains__, Const._operations)) and not any(
+                    map(line.startswith, Const._data_type)):
+                self.tokens[line] = {"Type": "Operation"}
+
+            else:
+                self.tokens[line] = {"Type": "None"}
+
+    def declarationToken(self, line: str) -> None:
+        """
+        this function takes the lines tokonized as a declaration line and add the variable with its value 
+        to variables_value dict
+
+        Args:
+            line (str): line contains variable to be declare 
+        """
+        value = 0
+        if line.__contains__("="):
+            value = line.split(" ")[-2]
+
+        if (any(map(line.__contains__, Const._operations))):
+            value = eval(value, Const.variables_value)
+
+        Const.variables_value[line.split(" ")[1]] = int(value)
--- SourceCode.py
+++ SourceCode.py
@@ -1,96 +1,10 @@
-
-
-
-from ast import Pass
-from lib2to3.pgen2.token import OP
-from tkinter import Variable
-from unittest import case
-
-
-class Const : 
-    DATATYPE=["int","float","double","long","char"]
-    OPERATIONS = ["+","-","*","/","++","--","*=","-=","/=","+="]
-    VARIABLES = {}
-class Data:
-    def __init__(self,file_name):
-        self.data = open(file_name).read()
-        self.lines = self.data.split("\n")
-        self.tokens = {}
-    def tokinizer(self):
-        for i in self.lines:
-            if i.startswith("//"):
-                self.tokens[i]= {"Type":"Comment"}
-            elif i.startswith("class"):
-                self.tokens[i]= {"Type":"Class"}
-            elif any(map(i.startswith,Const.DATATYPE)) and i.endswith(")"):
-                self.tokens[i]={"Type":"Function"}
-            elif i.startswith("cout<<"):
-                self.tokens[i]={"Type":"Print"}
-            elif any(map(i.startswith,Const.DATATYPE)) and (i.__contains__("=") or i.__contains__(";")):
-                self.tokens[i] = {"Type":"Declaration"}
-                self.declarationToken(i)
-            elif any(map(i.__contains__,Const.OPERATIONS)) and not any(map(i.startswith,Const.DATATYPE)):
-                self.tokens[i] = {"Type":"Operation"}
-            else :
-                self.tokens[i] = {"Type":"None"}
-
-    def declarationToken(self,i:str):
-        print("--------------------")
-        value = 0
-
-        if i.__contains__("="):
-            value = i.split(" ")[-2]
-        if (any(map(i.__contains__,Const.OPERATIONS))):
-            if(i.split(" ")[-2].__contains__("-")):
-                value1 = int(Const.VARIABLES[i.split(" ")[-2].split("-")[0]]["Value"])
-                value2 = int(Const.VARIABLES[i.split(" ")[-2].split("-")[1]]["Value"])
-                value = value1-value2
-
-        if i.split(" ")[1] in Const.VARIABLES.keys():
-            Const.VARIABLES[i.split(" ")[1]]["Value"]=value
-            Const.VARIABLES[i.split(" ")[1]]["Changed"]=False
-        else:
-            Const.VARIABLES[i.split(" ")[1]]={
-                "Value":value,
-                "Changed":False
-            }
-
-
-
-class DivideByZero:
-    def moveForword(data:Data):
-        temp_tokens = data.tokens
-        for i in data.lines:
-            if temp_tokens[i]["Type"]=="Function":
-                temp_tokens = data.tokens
-            if temp_tokens[i]["Type"]=="Operation":
-                if any(map(temp_tokens[i]["Value"].__contains__,Const.OPERATIONS)):
-                    pass
-                    
-
-
-                
-
-            
-     
-
-            
-
-
-
-
-
-        
-
-                  
-
-        
-    
+from Const import Const
+from Data import Data
+  
 def main():
     data = Data("code.txt")
     data.tokinizer()
-    print(Const.VARIABLES)
-    DivideByZero.moveForword(data)
+    print(Const.variables_value)
     pass
     
 if __name__ =="__main__":
