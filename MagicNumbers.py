6045de45cf8c26b44b86cace8babb4e8e500adba
yaseen <yaseen.asaliya22@gmail.com>
2022-03-08 23:52:17 +0200

    magic numbers handiling

--- MagicNumbers.py
+++ MagicNumbers.py
@@ -1,43 +1,41 @@
+from Const import Const
 
 class MagicNumbers:
-
+    result = []
     def __init__(self, tokens: list) -> None:
         self.tokens = tokens
-        self.Ifs = self.initialize_lines(tokens,'If')
-        self.returns = self.initialize_lines(tokens,'Return')
-        self.fors = self.initialize_lines(tokens,'For')
-        self.whiles = self.initialize_lines(tokens,'While')
-        self.calling_finction = self.initialize_lines(tokens,'calling_finction')
-
-
-    def implementation(self) -> None:
-        self.handle_magic_numbers(self.Ifs)
-        self.handle_magic_numbers(self.returns)
-        self.handle_magic_numbers(self.whiles)
-        self.handle_magic_numbers(self.fors)
-        self.handle_magic_numbers(self.calling_finction)
-
-    def initialize_lines(tokens,type) -> list:
-        temp = []
-        for key, value in tokens.items():
-            if value['Type'] == type:
-                temp.append(key)
-        return temp
-
-
-    def handle_magic_numbers(magic_type) -> None:
+    
+    def contain_digit(self,line) -> bool:
+        """
+        function that check the line if contain number or not.
+        """
+        containMagic = False
+        for cell in line:
+            if cell.isdigit():
+                containMagic = True
+        return containMagic
+        
+    def handle_magic_numbers(self, magic_type) -> None:
+        """
+        function that check each line in file if contain any type of magic 
+        number, in the end it will return all lines that contain magic number in list.
+        """
         containMagic = False
         for line in magic_type:
-            for digit in line:
-                if digit.isdigit():
-                    containMagic = True
-
-            if '"' in line and (line.count('"') % 2 == 0) and line.count('"') != 0:
-                containMagic = True
+            if line.__contains__('cout'):
+                if any(map(line.__contains__, Const._operations)):
+                    containMagic = self.contain_digit(line)
             
-        if containMagic == True:
-            print("this line contain magic number => ",line)
-
-
-
-
+            elif not line.__contains__('cout') and not any(map(line.startswith, Const._data_type)):
+                containMagic = self.contain_digit(line)
+                if '"' in line and (line.count('"') % 2 == 0) and line.count('"') != 0:
+                 containMagic = True
+                
+            elif any(map(line.startswith, Const._data_type)) and any(map(line.__contains__, Const._operations)):
+                containMagic = self.contain_digit(line)
+                
+            if containMagic == True:
+                print(line)
+                self.result.append(line)
+                containMagic == False
+        
