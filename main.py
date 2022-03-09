e7dcad1d74871cfced30d1e11e3bf207a1da0c4b
mohamad nasser <mohamadnasser198@outlook.com>
2022-03-09 21:55:01 +0200

    Add print Class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from modules.Data import Data
 from modules.Parameters import Parameters
 from modules.MagicNumbers import MagicNumbers
 from modules.UnReachableCode import UnReachableCode
+from modules.Print import Print
 from pprint import pprint
 
 
@@ -10,15 +11,25 @@ def main():
 
     data = Data("code.txt")
     data.tokinizer()
-    parameters = Parameters(data.tokens)
 
+    parameters = Parameters(data.tokens)
+    #solve the number of attributes
+    parameters.handle_three_parameters()
+    #solve the number of type and size match  when call function
     parameters.handle_attribute()
 
-    # handle more then three parameters
-
     magic_numbers = MagicNumbers(data.tokens)
-    print('handle_magic_numbers :')
-    magic_numbers.handle_magic_numbers(data.tokens)
-        
+    magic_numbers.handle_magic_numbers()
+
+    unreachable_code = UnReachableCode(data)
+    unreachable_code.body_of_functions()
+    unreachable_code.handle_alone_return()
+    unreachable_code.handle_if_return()
+    unreachable_code.handle_same_logic()
+
+    print = Print(data, magic_numbers, unreachable_code, parameters)
+    print.print_results()
+
+
 if __name__ == "__main__":
     main()
--- modules/MagicNumbers.py
+++ modules/MagicNumbers.py
@@ -1,11 +1,13 @@
 from modules.Const import Const
 
+
 class MagicNumbers:
     result = []
+
     def __init__(self, tokens: list) -> None:
         self.tokens = tokens
-    
-    def contain_digit(self,line) -> bool:
+
+    def contain_digit(self, line) -> bool:
         """
         function that check the line if contain number or not.
         """
@@ -14,28 +16,29 @@ class MagicNumbers:
             if cell.isdigit():
                 containMagic = True
         return containMagic
-        
-    def handle_magic_numbers(self, magic_type) -> None:
+
+    def handle_magic_numbers(self) -> None:
         """
         function that check each line in file if contain any type of magic 
         number, in the end it will return all lines that contain magic number in list.
         """
         containMagic = False
-        for line in magic_type:
+        for line in self.tokens:
             if line.__contains__('cout'):
                 if any(map(line.__contains__, Const._operations)):
                     containMagic = self.contain_digit(line)
-            
-            elif not line.__contains__('cout') and not any(map(line.startswith, Const._data_type)):
+
+            elif not line.__contains__('cout') and not any(
+                    map(line.startswith, Const._data_type)):
                 containMagic = self.contain_digit(line)
-                if '"' in line and (line.count('"') % 2 == 0) and line.count('"') != 0:
-                 containMagic = True
-                
-            elif any(map(line.startswith, Const._data_type)) and any(map(line.__contains__, Const._operations)):
+                if '"' in line and (line.count('"') % 2
+                                    == 0) and line.count('"') != 0:
+                    containMagic = True
+
+            elif any(map(line.startswith, Const._data_type)) and any(
+                    map(line.__contains__, Const._operations)):
                 containMagic = self.contain_digit(line)
-            
+
             if containMagic == True:
-                print(line)
                 self.result.append(line)
                 containMagic == False
-        
